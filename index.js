11734b657164fd1406fc86a5351f5ce13f793c84
dani110397 <50135171+dani110397@users.noreply.github.com>
2023-06-09 00:38:40 +0200

    cold + heating

--- index.js
+++ index.js
@@ -1,63 +1,69 @@
 const axios = require('axios');
+const wled = 'http://192.168.200.71/json/state'
 
-console.log(process.argv[2]);
-
-axios.get('http://mainsail.local/printer/objects/query?print_stats')
-    .then(res =>{
-        var state = res.data.result.status.print_stats.state;
+axios.get('http://mainsail.local/printer/objects/query?print_stats=state&extruder=temperature&heater_bed=temperature')
+    .then(result =>{
+        var heater_bed = result.data.result.status.heater_bed.temperature;
+        var extruder = result.data.result.status.extruder.temperature
+        var state = result.data.result.status.print_stats.state;
+        if(extruder < 50 && heater_bed < 35){
+            state = "cold";
+        }
+        if(state == "printing" && extruder < 200 && heater_bed < 50){
+            state = "heating";
+        }
         console.log(state)
         switch(state){
             case "printing":
-                console.log("Print active");
-                axios.post('http://192.168.200.71/json/state', {
-                    // on: 't',
+                axios.post(wled, {
                     ps: 7
                 })
             break;
 
             case "standby":
-                console.log("Print active");
-                axios.post('http://192.168.200.71/json/state', {
-                    // on: 't',
+                axios.post(wled, {
                     ps: 2
                 })
             break;
 
             case "cancelled":
-                console.log("Print active");
-                axios.post('http://192.168.200.71/json/state', {
-                    // on: 't',
+                axios.post(wled, {
                     ps: 5
                 })
             break;
 
             case "completet":
-                console.log("Print active");
-                axios.post('http://192.168.200.71/json/state', {
-                    // on: 't',
+                axios.post(wled, {
                     ps: 4
                 })
             break;
 
             case "error":
-                console.log("Print active");
-                axios.post('http://192.168.200.71/json/state', {
-                    // on: 't',
+                axios.post(wled, {
                     ps: 6
                 })
             break;
 
             case "paused":
-                console.log("Print active");
-                axios.post('http://192.168.200.71/json/state', {
-                    // on: 't',
+                axios.post(wled, {
                     ps: 3
                 })
             break;
 
+            case "cold":
+                axios.post(wled, {
+                    ps: 8
+                })
+            break;
+
+            case "heating":
+                axios.post(wled, {
+                    ps: 9
+                })
+            break;
+
             default:
-                axios.post('http://192.168.200.71/json/state', {
-                    // on: 't',
+                axios.post(wled, {
                     ps: 1
                 })
             break;
